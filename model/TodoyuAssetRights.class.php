9f82514c9ad50ff261fc42c68789fcf054f6ddd4
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-03-22 14:32:17 +0000

    * added TodoyuAssetRights class

---
+++ model/TodoyuAssetRights.class.php
@@ -0,0 +1,127 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 snowflake productions gmbh
+*  All rights reserved
+*
+*  This script is part of the todoyu project.
+*  The todoyu project is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License, version 2,
+*  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
+*  the Free Software Foundation;
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Asset rights functions
+ *
+ * @package		Todoyu
+ * @subpackage	Asset
+ */
+class TodoyuAssetRights {
+
+	/**
+	 * Deny access
+	 * Shortcut for asset
+	 *
+	 * @param	String		$right		Denied right
+	 */
+	private static function deny($right) {
+		TodoyuRightsManager::deny('asset', $right);
+	}
+
+
+
+	/**
+	 * Check if a person is allowed to see the asset
+	 *
+	 * @param	Integer		$idAsset
+	 * @return	Boolean
+	 */
+	public static function isSeeAllowed($idAsset) {
+		$idAsset	= intval($idAsset);
+		$asset		= TodoyuAssetManager::getAsset($idAsset);
+
+		$idParent	= $asset->getParentID();
+		$typeParent	= $asset->getParentType();
+
+		switch($typeParent) {
+			case 'task':
+				if ( TodoyuTaskRights::isSeeAllowed($idParent) ) {
+					if ( allowed('asset', 'asset:seeAll') ) {
+						return true;
+					}
+				}
+				break;
+		}
+
+		return false;
+	}
+
+
+
+	/**
+	 * Check if a person is allowed to delete the asset
+	 *
+	 * @param	Integer		$idAsset
+	 * @return	Boolean
+	 */
+	public static function isDeleteAllowed($idAsset) {
+		$idAsset	= intval($idAsset);
+		$asset		= TodoyuAssetManager::getAsset($idAsset);
+
+		$idParent	= $asset->getParentID();
+		$typeParent	= $asset->getParentType();
+
+		switch($typeParent) {
+			case 'task':
+				if ( TodoyuTaskRights::isSeeAllowed($idParent) ) {
+					if ( allowed('asset', 'asset:delete') ) {
+						return true;
+					}
+				}
+				break;
+		}
+
+		return false;
+	}
+
+
+
+	/**
+	 * Restrict access to person which are allowed to see the asset
+	 *
+	 * @todo	state deny message more precisely
+	 * @param	Integer		$idAsset
+	 */
+	public static function restrictSee($idAsset) {
+		$idAsset	= intval($idAsset);
+
+		if( ! self::isSeeAllowed($idAsset) ) {
+			self::deny('asset:see');
+		}
+	}
+
+
+
+	/**
+	 * Restrict access to persons which are allowed to delete assets from this task
+	 *
+	 * @todo	state deny message more precisely
+	 * @param	Integer		$idComment
+	 */
+	public static function restrictDelete($idAsset) {
+		if( ! self::isDeleteAllowed($idAsset) ) {
+			self::deny('asset:delete');
+		}
+	}
+
+}
+?>
