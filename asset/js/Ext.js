5fdb6317f0a0ba16ef4271f90d1b7318bd561527
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-03-28 15:18:52 +0000

    * added: notification after deletion of asset file

--- asset/js/Ext.js
+++ asset/js/Ext.js
@@ -145,6 +145,8 @@ Todoyu.Ext.assets = {
 		var idTask	= response.getTodoyuHeader('idTask');
 		var label	= response.getTodoyuHeader('tabLabel');
 
+		Todoyu.Notification.notifySuccess('[LLL:assets.ext.delete.notifiy.success]');
+
 		this.setTabLabel(idTask, label);
 		this.updateTab(idTask);
 	},
